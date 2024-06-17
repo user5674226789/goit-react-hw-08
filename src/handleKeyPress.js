af29275b9b20aff9a90c6457f8c3fda40589fadf
user5674226789 <148801262+user5674226789@users.noreply.github.com>
2024-06-17 12:22:17 +0300

    add

--- src/handleKeyPress.js
+++
@@ -1,6 +0,0 @@
-export const handleKeyPress = (event) => {
-  const charCode = event.charCode;
-  if ((charCode < 48 || charCode > 57) && charCode !== 45) {
-    event.preventDefault();
-  }
-};
--- src/redux/filters/selectors.js
+++
@@ -1,1 +0,0 @@
-export const selectNameFilter = (state) => state.filter.name;
